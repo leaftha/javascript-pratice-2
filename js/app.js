7a03414688c474bd1caf707a275c76ce05b8410f
leaftha <vltpcks@gmail.com>
2020-06-29 02:42:06 +0900

    2d context(have problem)

--- js/app.js
+++ js/app.js
@@ -1,4 +1,8 @@
 const canvas = document.getElementById("jsCanvas");
+const ctx = canvas.getContext("2d");
+
+ctx.strokeStyle = "#2c2c2c";
+ctx.lineWidt = 2.5;
 
 let painting = false;
 
@@ -6,13 +10,20 @@ function stopPainting() {
   painting = false;
 }
 
+function startPainting() {
+  painting = true;
+}
+
 function onMouseMove(event) {
   const x = event.offsetX;
   const y = event.offsetY;
-}
-
-function onMouseDown(event) {
-  painting = true;
+  if (!painting) {
+    ctx.beginPath();
+    ctx.moveTo(x, y);
+  } else {
+    ctx.lineTo(x, y);
+    ctx.stroke();
+  }
 }
 
 function onMouseUp(event) {
@@ -21,7 +32,7 @@ function onMouseUp(event) {
 
 if (canvas) {
   canvas.addEventListener("mousemove", onMouseMove);
-  canvas.addEventListener("mousedown", onMouseDown);
-  canvas.addEventListener("mouseup", onMouseUp);
+  canvas.addEventListener("mousedown", startPainting);
+  canvas.addEventListener("mouseup", stopPainting);
   canvas.addEventListener("mouseleave", stopPainting);
 }
